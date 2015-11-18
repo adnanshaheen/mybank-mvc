392283ad8e938a80df448c68ddac0d5bd90fcf6b
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-18 03:26:53 -0500

    add the XferChkToSavModel class

---
+++ MyBankMVC15/Models/XferChkToSavModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBankMVC15.Models
+{
+    public class XferChkToSavModel
+    {
+        public double CheckingBalance { get; set; }
+        public double SavingBalance { get; set; }
+        public double AmountTransfer { get; set; }
+        public string Status { get; set; }
+    }
+}
